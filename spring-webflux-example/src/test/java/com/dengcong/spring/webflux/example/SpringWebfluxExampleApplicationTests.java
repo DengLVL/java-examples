b7cfc02793421a27e35422ce07377cd5f7b312e9
“DengLVL“ <1159827211@qq.com>
2019-02-28 16:28:43 +0800

    代码

---
+++ spring-webflux-example/src/test/java/com/dengcong/spring/webflux/example/SpringWebfluxExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dengcong.spring.webflux.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringWebfluxExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
